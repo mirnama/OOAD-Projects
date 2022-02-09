1fab0862183b3b8f3b41baa2eb0d33804b3ee1de
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-09 12:25:38 -0700

    Update Accessories.java

--- project2_b/src/Item/Accessories.java
+++ project2_b/src/Item/Accessories.java
@@ -1,4 +1,3 @@
-package Accessories;
 public class Accessories extends Item {
     Accessories(String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
         super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
