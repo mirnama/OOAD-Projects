11f130b51d96b6eb82925faa79d8e8d7f338e781
Tommy <tmahre26@gmail.com>
2022-02-09 11:01:09 -0700

    Staff Changes

--- project2_b/src/Main.java
+++ project2_b/src/Main.java
@@ -1,11 +1,13 @@
-
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 
+import Staff.*;
 
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello!");
-
+        Store store = new Store();
+        ArrayList<Staff> hired = store.getStaffMembers();
+        System.out.println(hired.get(0).getEmployeeName());
     }
 }
