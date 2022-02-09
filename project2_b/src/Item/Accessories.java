9547d38d3d1c174f5bf858374c72eb25ac1d3c73
Mirna <70171117+mirnama@users.noreply.github.com>
2022-02-09 13:22:37 -0700

    Revert "merage"

---
+++ project2_b/src/Item/Accessories.java
@@ -0,0 +1,5 @@
+public class Accessories extends Item {
+    Accessories(String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
+        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
+    }
+}
