798a3252cf846b8f613462aefe920fa48fb38d93
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-09 12:44:09 -0700

    Delete PracticeAmps.java

--- project2_b/src/Item/PracticeAmps.java
+++
@@ -1,9 +0,0 @@
-import Accessories.Accessories;
-
-public class PracticeAmps extends Accessories {
-    private int wattage;
-    public PracticeAmps(int wattage, String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
-        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
-        this.wattage = wattage;
-    }
-}
