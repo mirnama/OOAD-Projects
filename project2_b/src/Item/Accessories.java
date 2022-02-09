077778b7bd09412858ccfec87b5c09631989bc33
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-09 12:43:46 -0700

    Delete Accessories.java

--- project2_b/src/Item/Accessories.java
+++
@@ -1,5 +0,0 @@
-public class Accessories extends Item {
-    Accessories(String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
-        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
-    }
-}
