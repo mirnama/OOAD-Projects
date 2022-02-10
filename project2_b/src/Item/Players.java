9fd1573b4a22b02e952d1d4afd7423ce4af6c7ea
mirnama <mima4642@colorado.edu>
2022-02-09 21:10:24 -0700

    Added Players Types

--- project2_b/src/Item/Players.java
+++
@@ -1,7 +0,0 @@
-package Item;
-
-public class Players extends Item{
-    public Players (String itemName, int itemCondition, Float purchasePrice, Float listPrice, Float salePrice) {
-        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
-    }
-}
---
+++ project2_b/src/Item/RecordPlyer.java
@@ -0,0 +1,7 @@
+package Item;
+
+public class RecordPlyer extends Item{
+    public RecordPlyer (String itemName, int itemCondition, Float purchasePrice, Float listPrice, Float salePrice) {
+        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
+    }
+}
