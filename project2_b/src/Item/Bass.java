f707083362a19b5a4d6b3d49130197b35f843938
Mirna <70171117+mirnama@users.noreply.github.com>
2022-02-09 22:28:07 -0700

    Update Bass.java

--- project2_b/src/Item/Bass.java
+++ project2_b/src/Item/Bass.java
@@ -2,7 +2,7 @@ package Item;
 
 public class Bass extends Stringed{
 
-    public Bass(Boolean isElectric, String itemName, int itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
+    public Bass(Boolean isElectric, int itemName, int itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
 
         super(isElectric, itemName, itemCondition, purchasePrice, listPrice, salePrice);
     }
