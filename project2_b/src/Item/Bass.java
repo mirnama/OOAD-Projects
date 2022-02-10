fe97d15170578cc480721e6f79ae8a828c339c27
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-09 19:33:11 -0700

    Delete Bass.java

--- project2_b/src/Item/Bass.java
+++
@@ -1,7 +0,0 @@
-package Item;
-
-public class Bass extends Stringed{
-    Bass(Boolean isElectric, String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
-        super(isElectric, itemName, itemCondition, purchasePrice, listPrice, salePrice);
-    }
-}
