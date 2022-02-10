0ccbf1792629fc3ac62aa5fc188111bda80913b4
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-09 19:33:18 -0700

    Delete Clothing.java

--- project2_b/src/Item/Clothing.java
+++
@@ -1,7 +0,0 @@
-package Item;
-
-public class Clothing extends Item{
-    Clothing (String itemName, String itemCondition, Float purchasePrice, Float listPrice, Float salePrice){
-        super( itemName, itemCondition, purchasePrice, listPrice, salePrice);
-    }
-}
