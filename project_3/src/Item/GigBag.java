aa0dbbdd898048529582e2b40d34b4d788823d0b
mirnama <mima4642@colorado.edu>
2022-02-21 19:31:51 -0700

    Creat new subclass

---
+++ project_3/src/Item/GigBag.java
@@ -0,0 +1,7 @@
+package Item;
+
+public class GigBag extends Accessories {
+    public GigBag (String itemName, int itemCondition, double purchasePrice, double listPrice, double salePrice) {
+        super(itemName, itemCondition, purchasePrice, listPrice, salePrice);
+    }
+}
