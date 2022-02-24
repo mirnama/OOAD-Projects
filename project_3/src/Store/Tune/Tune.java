5f22cbcbfc4ce24ccb5d7b8144a1b2b84420562a
Nare6880 <49699188+Nare6880@users.noreply.github.com>
2022-02-23 21:40:16 -0700

    Delete project_3/src/Store/Tune directory
    messed up

--- project_3/src/Store/Tune/Tune.java
+++
@@ -1,8 +0,0 @@
-package Store.Staff.Tune;
-import Item.*;
-public interface Tune {
-    public abstract void do_tune(Stringed item);
-    public abstract void do_tune(Wind item);
-    public abstract void do_tune(Player item);
-
-}
