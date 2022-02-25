8d2579df18dd0cf35ed5f13a741ebe9a52a73373
Tommy <tmahre26@gmail.com>
2022-02-25 12:27:09 -0700

    Print out statements $amounts

--- project_3/src/Store/Staff/Jobs/GoToBank.java
+++ project_3/src/Store/Staff/Jobs/GoToBank.java
@@ -17,7 +17,7 @@ public class GoToBank implements Job {
     public void do_job(Store s, Staff p) {
         System.out.println("Withdrawing $1000 from bank...");
         s.setCashReg(s.getCashReg() + 1000);
-        System.out.println("Register now has $"+s.getCashReg());
+        System.out.println("Register now has $"+String.format("%.2f", s.getCashReg()));
         notifyObservers("$"+String.format("%.2f", s.getCashReg())+" in register.");
     }
     public void registerObserver(Logger o) {
